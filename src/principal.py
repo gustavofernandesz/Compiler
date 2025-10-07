d499910e815734ad56f76640b005093c479f68e8
gustavofernandesz <gustavofernandesinc@gmail.com>
2025-10-07 11:37:28 -0300

    adicionando tokes e saida para tabela de símbolos

--- src/principal.py
+++ src/principal.py
@@ -17,4 +17,38 @@ reservadas={
     'sameAs',
     'differentFrom',
     'import'
+    'kind'
+    'role'
+    'phase'
 }
+type_data=[
+    'number',
+    'string',
+    'boolean',
+    'date',
+    'time',
+    'datetime'
+]
+
+tokens=[
+
+]+ list(set(reservadas.values()))
+
+
+t_LPAREN = r'\('      # abre parêntese
+t_RPAREN = r'\)'      # fecha parêntese
+t_LBRACE = r'\{'      # abre chave
+t_RBRACE = r'\}'      # fecha chave
+t_COMMA = r'\,'       # vírgula
+t_ignore = ' \n\t'      # ignora espaços e tabulações
+t_MAIORIGUAL = r'>='  # operador >=
+t_MENORIGUAL = r'<='  # operador <=
+t_MAIOR = r'>'        # operador >
+t_MENOR = r'<'        # operador <
+t_CONTEM = r'<>--'
+t_CONTIDO = r'--<>'
+t_ASTERISCO = r'\*'
+t_ARROBA = r'@'
+
+
+saida = [] # tabela de simbolos
