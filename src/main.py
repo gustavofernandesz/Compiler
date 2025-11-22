f37534256280b78c88c7639e7ddc34db26e8f61e
GustavoFernandes413 <gustavo.marreira@alunos.ufersa.edu.br>
2025-11-22 15:56:08 -0300

    Criação do menu de seleção entre análise léxica ou sintática

--- src/main.py
+++ src/main.py
@@ -8,22 +8,34 @@ caminho_exemplo = os.path.join(diretorio_atual, 'example.tonto')
 with open(caminho_exemplo, 'r') as f:
     codigo = f.read()
 
-lexer = analisador_lexico.build()
+print("="*60)
+print("Selecione o tipo de análise:")
+print("[1] - Análise léxica")
+print("[2] - Análise sintática")
+opcao = input("Escolha 1/2: ").strip()
+print("="*60)
+if opcao == "1" :
 
-tabela, contagem = analisador_lexico.lex_table(codigo, lexer)
+    lexer = analisador_lexico.build()
 
-print(f"{'Linha':<8} {'Token':<25} {'Valor'}")
-print("=" * 60)
-for linha, tipo, valor in tabela:
-    print(f"{linha:<8} {tipo:<25} {valor}")
+    tabela, contagem = analisador_lexico.lex_table(codigo, lexer)
 
-print(f"\nTotal de tokens: {len(tabela)}")
-print("\nContagem por tipo:")
-print("=" * 30)
-for tipo, qtd in contagem.items():
-    print(f"{tipo:<25}: {qtd}")
+    print(f"{'Linha':<8} {'Token':<25} {'Valor'}")
+    print("=" * 60)
+    for linha, tipo, valor in tabela:
+        print(f"{linha:<8} {tipo:<25} {valor}")
 
-resultado, ast = analisador_sintatico.parse(codigo)
+    print(f"\nTotal de tokens: {len(tabela)}")
+    print("\nContagem por tipo:")
+    print("=" * 30)
+    for tipo, qtd in contagem.items():
+        print(f"{tipo:<25}: {qtd}")
 
-analisador_sintatico.gerar_tabela_sintese()
-analisador_sintatico.gerar_relatorio_erros()
+elif opcao == "2":
+    resultado, ast = analisador_sintatico.parse(codigo)
+    analisador_sintatico.gerar_tabela_sintese()
+    analisador_sintatico.gerar_relatorio_erros()
+    print("Esta é uma análise simplificada - para a análise completa, referir-se ao arquivo parser.out")
+
+else:
+    print("Opção inválida!")
